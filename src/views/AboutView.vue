815587c66af54df2aeee71b2d0bc1b1ff7c72582
davidcao <davidcao6426@gmail.com>
2024-05-16 23:55:03 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import axios from 'axios'
+import{reactive, onMounted} from 'vue';
+
+const data = reactive({
+  newsdata:'',
+})
+
+axios.defaults.baseURL = 'http://localhost:8080'
+axios.get('/api/user/user',{
+  params: {
+    id: "176"
+  }
+})
+    .then(function (response) {
+      // handle success
+      console.log("success");
+      console.log(response.data);
+      data.newsdata = response.data
+      console.log(response.data.userId);
+      console.log(response.data.email);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log("error");
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>{{ data.newsdata }}</h1>
+    <h1>{{ data.newsdata.userId }}</h1>
+    <h1>{{ data.newsdata.email }}</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8080'
+
+function addUser(){
+  axios.post('/api/user/user',{
+    userId: "5564645645",
+    username: "First",
+    email: "davidcao6426@gmail.com",
+    password: "hl654d93",
+    coverImage: "123",
+    biography: "text_text"
+  })
+      .then(function (response) {
+        // handle success
+        console.log("success");
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("error");
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+}
+
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
